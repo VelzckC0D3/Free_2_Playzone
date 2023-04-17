8860e5b3b0ea9e6dc4f5d721971c6c2e7d5bea34
Marco Almada <43345162+MarcoDDM@users.noreply.github.com>
2023-04-17 17:32:18 -0300

    Linters fix mod

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import _ from 'lodash';
 import './style.css';
 
- function component() {
+function component() {
   const element = document.createElement('div');
-   // Lodash, now imported by this script
+  // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
   return element;
- }
+}
 
- document.body.appendChild(component());
+document.body.appendChild(component());
