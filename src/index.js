98d9bd26ffa5000d1dfb6f8129d09ec812791fd5
Marco Almada <43345162+MarcoDDM@users.noreply.github.com>
2023-04-19 17:51:04 -0300

    Change for linters

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import './style.css';
+
 const gameCardsElement = document.querySelector('#game-cards');
 const options = {
   method: 'GET',
   headers: {
     'X-RapidAPI-Key': '16a429eb7fmshb30918cd461ee6dp1673e1jsn62d7843ea7e3',
-    'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com'
-  }
+    'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com',
+  },
 };
 
 fetch('https://free-to-play-games-database.p.rapidapi.com/api/filter?tag=3d.mmorpg.fantasy.pvp&platform=pc', options)
-  .then(response => response.json())
-  .then(data => {
-    for (let i = 0; i < data.length; i++) {
+  .then((response) => response.json())
+  .then((data) => {
+    for (let i = 0; i < data.length; i=i+1) {
       const game = data[i];
       const gameCardHTML = `
         <div class="game-card">
@@ -25,4 +26,4 @@ fetch('https://free-to-play-games-database.p.rapidapi.com/api/filter?tag=3d.mmor
       gameCardsElement.insertAdjacentHTML('beforeend', gameCardHTML);
     }
   })
-  .catch(err => console.error(err));
+  .catch((err) => console.error(err));
